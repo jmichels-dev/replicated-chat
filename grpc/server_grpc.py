62238194f8f8fc7a1c951eee3d0c5765ae438a62
Joshua Michels <joshuamichels@dhcp-10-250-226-222.harvard.edu>
2023-04-08 13:40:10 -0400

    Created snapshot thread

--- grpc/server_grpc.py
+++ grpc/server_grpc.py
@@ -1,16 +1,23 @@
 from concurrent import futures
 import logging
+import time
+import threading
+import csv
 
 import grpc
 import chat_pb2
 import chat_pb2_grpc
 import helpers_grpc
 
+
+SNAPSHOT_INTERVAL = 5 # seconds
+
 class ChatServicer(chat_pb2_grpc.ChatServicer):
 
     def __init__(self):
         # {username : [loggedOnBool, [messageQueue]]}
-        self.clientDict = {}
+        #self.clientDict = {}
+        self.clientDict = {'test1': [True, ["hello1", "hello2"]]}
 
     def SignInExisting(self, username, context):
         eFlag, msg = helpers_grpc.signInExisting(username.name, self.clientDict)
@@ -47,10 +54,16 @@ class ChatServicer(chat_pb2_grpc.ChatServicer):
     def Delete(self, username, context):
         self.clientDict.pop(username.name)
         return chat_pb2.Payload(msg="Goodbye!\n")
+    
+    def Snapshot(self):
+        with open('test.csv', 'w', newline = '') as testfile:
+                rowwriter = csv.writer(testfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+                for key, val in self.clientDict:
+                    rowwriter.writerow([key] + val)
 
 
 def serve():
-    ip = '10.250.31.89'
+    ip = '10.250.226.222'
     port = '8080'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     chat_pb2_grpc.add_ChatServicer_to_server(ChatServicer(), server)
@@ -60,6 +73,21 @@ def serve():
     server.wait_for_termination()
 
 
+def snapshot():
+    print("inside snapshot")
+    print(ChatServicer)
+    SERVER_TIME = time.time()
+    while True:
+        if time.time() - SERVER_TIME > SNAPSHOT_INTERVAL:
+            ChatServicer.Snapshot(self=ChatServicer)
+            SERVER_TIME = time.time()
+    
+
+
+
 if __name__ == '__main__':
     logging.basicConfig()
+    # spin off thread for snapshotting state
+    snapshot_thread = threading.Thread(target=snapshot, args=(), daemon=True)
+    snapshot_thread.start()
     serve()
